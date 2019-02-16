f3bf19f154af05036e21cd81e04c9d810768acfe
Forrest <lljsg123123@gmail.com>
2019-02-16 14:09:47 -0800

    created guard

--- Player.gd
+++ Player.gd
@@ -1,22 +1,29 @@
 extends KinematicBody2D
 
-const SPEED = 200
+onready var player = get_node('/root/Node/Player')
+const SPEED = 300
 const R_MINUS = 1
 
 var motion = Vector2()
+var speed = 100
+var size = 10
+var invisibility = 100
+
 
 func move():
 	if Input.is_key_pressed(KEY_D):
 		motion.x = 1
 	elif Input.is_key_pressed(KEY_A):
 		motion.x = -1
-	elif Input.is_key_pressed(KEY_W):
+	else:
+		motion.x = 0
+	if Input.is_key_pressed(KEY_W):
 		motion.y = -1
 	elif Input.is_key_pressed(KEY_S):
 		motion.y = 1
 	else:
 		motion.y = 0
-		motion.x = 0
+
 	
 	motion = motion.normalized() * SPEED
 	
@@ -27,11 +34,3 @@ func move():
 func _physics_process(delta):
 	move()
 	motion = move_and_slide(motion)
-
-
-func eat():
-	pass
-	
-func run():
-	pass
-	
