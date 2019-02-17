09e8b7f54d47ff3354d7f5acf039247cceae0734
Kaitian Xie <xkaitian@gmail.com>
2019-02-17 10:56:56 -0800

    Implement timer

--- Game.gd
+++ Game.gd
@@ -1,7 +1,7 @@
 extends Node
 
 onready var paths = preload("res://Paths.gd")
+onready var timer  = get_node(paths.TIMER)
 
-func _ready():
-    var timer  = get_node(paths.TIMER)
-
+func _process(delta):
+    timer.get_node('Label').text = str(int(timer.get_time_left())) + 's'
