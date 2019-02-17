95e63e36da2f492fc3468b382554b0ee7dfe8e3d
Kaitian Xie <xkaitian@gmail.com>
2019-02-17 02:46:53 -0800

    Finish pause menu

--- interface/pause/Pause.gd
+++ interface/pause/Pause.gd
@@ -3,7 +3,6 @@ extends Control
 func _input(event):
     if event.is_action_pressed('pause'):
         var new_pause_state = not get_tree().paused
-        print(new_pause_state)
         get_tree().paused = new_pause_state
         visible = new_pause_state
 
