d4420b0d3542f786ab30da75b1aa65f30d422b6d
Kaitian Xie <xkaitian@gmail.com>
2019-02-16 18:16:38 -0800

    Code cleanup

--- Utils.gd
+++ Utils.gd
@@ -1,6 +1,6 @@
-const GAME_OVER_PATH = '/root/Node/GameOver'
-const PLAYER = '/root/Node/Level_1/player'
-const GUARDS = '/root/Node/Level_1/guards'
+const GAME_OVER_PATH = '/root/Game/GameOver'
+const PLAYER = '/root/Game/Level_1/player'
+const GUARDS = '/root/Game/Level_1/guards'
 
 static func cartesian_to_isometric(vector):
     return Vector2(vector.x - vector.y, (vector.x + vector.y) / 2)
